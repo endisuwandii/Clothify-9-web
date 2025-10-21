130fc34d2089493ddd1d7970f540d149ee4b80a8
endisuwandi <endisuwandi64@gmail.com>
2025-10-21 07:30:35 +0700

    chore: setup client side and fethcing data

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import useSWR from "swr";
+import { fetcher } from "./lib/fetcher";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useSWR(`${import.meta.env.VITE_BACKEND_API_URL}/products`, fetcher);
+
+  if (error) return <div>Failed to load products</div>;
+  if (isLoading) return <div>Loading products...</div>;
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Clothify-Endi</h1>
+      <ul>
+        {products.map((product: any) => {
+          return (
+            <li key={product.id}>
+              <img src={product.imageUrl} alt={product.name} width={300} />
+              <h2>{product.name}</h2>
+            </li>
+          );
+        })}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
