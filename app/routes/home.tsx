4d03433d5a59404b4d48e7d8c91c87f32ae411bd
endisuwandi <endisuwandi64@gmail.com>
2025-10-21 18:37:06 +0700

    chore: setup home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,16 +1,43 @@
+import type { Products } from "~/modules/product/type";
 import type { Route } from "./+types/home";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "Clothify" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: "Merchandise from the zoo." },
   ];
 }
 
-export default function Home() {
+export async function clientLoader() {
+  const response = await fetch(
+    `${import.meta.env.VITE_BACKEND_API_URL}/products`
+  );
+  const products: Products = await response.json();
+
+  return { products };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const { products } = loaderData;
+
   return (
     <div>
-      <h1>Clothify</h1>{" "}
+      <h1>Clothify</h1>
+
+      <ul className="grid grid-cols-3">
+        {products.map((product) => {
+          return (
+            <li key={product.id}>
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                className="size-52"
+              />
+              <h2>{product.name}</h2>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
